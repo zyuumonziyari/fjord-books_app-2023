a6ed22017ca62de2c536676830ccb6086618b052
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-03-28 23:24:54 +0900

    URLにlocalパラメータを含ませ、日本語と英語のページの切り替えを可能にした。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :books
+  # scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+  resources :books, path: '(:locale)/books',
+                    locale: /#{I18n.available_locales.map(&:to_s).join("|")}/
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
