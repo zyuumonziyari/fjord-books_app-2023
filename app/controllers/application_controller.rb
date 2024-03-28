7f0ffa27ac7a7386af9c4a32a0c7cc8a0ba9d990
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-03-28 23:28:49 +0900

    localeをパラメータで取得できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :current_locale
+
+  def current_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: current_locale }
+  end
 end
