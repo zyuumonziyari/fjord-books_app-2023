f05ce938855e1cd882e366c5e4e44c6a66787b97
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-04 16:01:09 +0900

    Userモデルに必要なカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_04_063753) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_04_065930) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_04_063753) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,8 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_04_063753) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.string "post_number"
+    t.text "profile_text"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
