25f019bb25703f086f485b22c891ae50a8638f85
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-04 18:56:42 +0900

    userのindexとshowページのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users', to: 'user#index'
+  get 'user/show'
+
   devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
