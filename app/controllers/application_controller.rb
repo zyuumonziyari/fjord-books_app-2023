871639f86ccced08d6e0cb6a4e28110738a18b6e
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-09 19:52:25 +0900

    ログイン後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    
+    protected
+  
+    def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || users_path
+    end 
 end
