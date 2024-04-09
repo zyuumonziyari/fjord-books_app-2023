7c929e67526d51bb1e54cdd45fe67872b9170a46
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-09 21:26:13 +0900

    新規登録とアカウント更新後のリダイレクト先の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,19 @@
 
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_account_update_params, only: [:update]
     
     protected
-  
     def after_sign_in_path_for(resource_or_scope)
       stored_location_for(resource_or_scope) || users_path
     end 
 
     def after_sign_out_path_for(scope)
       stored_location_for(scope) || new_user_session_path
+    end
+
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text])
     end
 
 end
