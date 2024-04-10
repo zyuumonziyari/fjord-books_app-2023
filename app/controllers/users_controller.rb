e449b6c04380fad8f01a0bb73501b115290202c8
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-10 15:31:43 +0900

    ユーザー詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
   end
-
+  
   def show
   end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
