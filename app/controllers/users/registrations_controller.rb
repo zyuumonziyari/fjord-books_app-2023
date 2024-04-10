72b123d97090ed297dbd2e3bc0f345b9efba94aa
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-10 20:33:14 +0900

    ユーザー登録後のページ遷移先をアカウント編集画面に変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    users_path(resource)
+    edit_user_registration_path(resource)
   end
 
   # The path used after sign up for inactive accounts.
