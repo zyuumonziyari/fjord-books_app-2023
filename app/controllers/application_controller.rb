1226320bace81c7804535a5bdf6c28b2e64c4a2d
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-11 17:52:39 +0900

    不要なbeforeアクションの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    before_action :configure_account_update_params, only: [:update]
     
     protected
     def after_sign_in_path_for(resource_or_scope)
@@ -11,10 +10,6 @@ class ApplicationController < ActionController::Base
 
     def after_sign_out_path_for(scope)
       stored_location_for(scope) || new_user_session_path
-    end
-
-    def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :address, :post_number])
     end
 
 end
