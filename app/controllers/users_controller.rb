8294c009aacd229f42187b2db42ef2a8975a304f
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-14 22:29:52 +0900

    文字列リテラルの凍結指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: %i[show]
 
   def index
     @users = User.order(updated_at: :desc, id: :desc).page(params[:page])
   end
-  
-  def show
-  end
+
+  def show; end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
-
 end
