3f653200c391311b63518dcc1a421d3f63385eb9
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-14 22:15:48 +0900

    updateの受け取るパラメーターをusersコントローラーで指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    
-    protected
-    def after_sign_in_path_for(resource_or_scope)
-      stored_location_for(resource_or_scope) || users_path
-    end 
+  before_action :authenticate_user!
 
-    def after_sign_out_path_for(scope)
-      stored_location_for(scope) || new_user_session_path
-    end
+  protected
 
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || users_path
+  end
+
+  def after_sign_out_path_for(scope)
+    stored_location_for(scope) || new_user_session_path
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: %i[update]
 
   # GET /resource/sign_up
   # def new
@@ -20,14 +20,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  #def update
-  #  if current_user.update(resource_params)
-  #    redirect_to users_path, notice: 'Your account was successfully updated.'
-  #  else
-  #    render :edit
-  #  end
-  #end
-  
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # DELETE /resource
   # def destroy
@@ -61,10 +70,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_text, :address, :post_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile_text address post_number])
   end
 
   def after_update_path_for(resource)
-    users_path(resource)
+    user_path(resource)
   end
 end
