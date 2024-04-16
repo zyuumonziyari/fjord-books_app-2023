eea1258dc183942f8ef759214cc399ddaa5da230
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-16 11:38:13 +0900

    protectedを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  protected
-
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || users_path
   end
