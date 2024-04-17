84df689654fec30943c290ca6ca73a3339d236b9
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-17 15:08:10 +0900

    サムネイル時の画像サイズの指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [300, 300]
+  end
 end
