d77f52a84946d1a458791226d4d0208e5f1e14c9
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-18 16:10:26 +0900

    画像サイズのリサイズ設定とバリデーションいよる画像ファイルの指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,11 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(updated_at: :desc, id: :desc).page(params[:page])
+    @users = User.with_attached_avatar.order(updated_at: :desc, id: :desc).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-
-
-## userのCRUD操作に関わる部分はdeviseコントローラー？？
-
-  def new
-    @user = User.new
-  end
-
-
-  def create
-    @user = User.new(user_params)
-    session[:user_id] = user.id
-    if @user.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [300, 300]
   end
+  validate :avatar_content_type
+  
+  def avatar_content_type
+    if avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:avatar, 'ファイル形式が、JPEG, PNG, GIF以外になってます。ファイル形式をご確認ください。')
+    end
+  end
+  
 end
