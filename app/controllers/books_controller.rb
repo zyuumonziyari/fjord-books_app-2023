c9e5a3ef59b3892692a79a5080b642460e308614
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-20 16:28:25 +0900

    本の作成にuserを関連付けた。json形式の指定を削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,40 +21,27 @@ class BooksController < ApplicationController
 
   # POST /books or /books.json
   def create
-    @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human) }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /books/1 or /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human) }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+  def updat
+    if @book.update(book_params)
+      redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
-
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human)
   end
 
   private
