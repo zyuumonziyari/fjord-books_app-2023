5dd64d317292de4ca4d1f295c1412fb8c750775f
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-20 15:16:18 +0900

    ユーザー詳細ページで、そのユーザーの日報一覧を閲覧できるようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,40 +21,27 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
-
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    @report = current_user.reports.build(report_params)
+    if @report.save
+      redirect_to report_url(@report), notice: "Report was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: "Report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end    
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
-
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: "Report was successfully destroyed."
   end
 
   private
