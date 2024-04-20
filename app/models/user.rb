450338869e5ebfdfd5ee46792aca514320ffee7a
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-20 16:23:49 +0900

    userとbookの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
+  has_many :books, dependent: :destroy
   has_many :reports, dependent: :destroy
 end
