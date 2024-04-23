0d181c2a0fad2cfaf9a96835aa01c8c4991f5f2e
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 00:07:31 +0900

    翻訳ファイルのlocales直下への移動に伴い、サブディレクトリ以下を読み込むコードを削除

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
