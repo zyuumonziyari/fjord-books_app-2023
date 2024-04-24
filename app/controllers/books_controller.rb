b0ca26096da11b574126642e8b377101407b4a3f
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 20:51:14 +0900

    不要なコメントアウトを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,23 +3,18 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books or /books.json
   def index
     @books = Book.order(updated_at: :desc, id: :desc).page(params[:page])
   end
 
-  # GET /books/1
   def show; end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit; end
 
-  # POST /books
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -29,7 +24,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
   def update
     if @book.update(book_params)
       redirect_to @book, notice: t('controllers.common.notice_update', title: @book.title)
@@ -38,7 +32,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1
   def destroy
     @book.destroy
     redirect_to books_path, notice: t('controllers.common.notice_destroy', title: @book.title)
@@ -46,12 +39,10 @@ class BooksController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_book
     @book = Book.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def book_params
     params.require(:book).permit(:title, :memo, :author, :picture)
   end
