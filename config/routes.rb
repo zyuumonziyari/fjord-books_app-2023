2990da541e1db53c65368ae87febbf858e19805d
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 18:58:50 +0900

    不要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { registrations: 'users/registrations'}
-  put "/users" => "users#update"
   
-  resources :users
+  resources :users, only: %i[index show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
