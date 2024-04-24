ef4a0f81c0990bdf1b6e9b16ab0b068c01006b15
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 18:43:29 +0900

    rubocopの警告に従い、引数を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,18 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_account_update_params, if: :devise_controller?
 
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || users_path
   end
 
-  def after_sign_out_path_for(scope)
+  def after_sign_out_path_for(_resource_name)
     new_user_session_path
+  end
+
+  def configure_account_update_params
+    keys = %i[name self_introduction address postal_code]
+    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 end
