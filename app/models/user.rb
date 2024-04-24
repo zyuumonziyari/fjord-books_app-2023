140a2a4addb976038def44789b9287c9dd4d5d25
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 23:28:55 +0900

    可読性を高める為、条件式を整える

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :avatar_content_type
+
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [300, 300]
   end
-  validate :avatar_content_type
 
   def avatar_content_type
-    errors.add(:avatar, :avatar_content_type) if avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+    if avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:avatar, :avatar_content_type)
+    end
   end
 end
