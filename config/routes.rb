7068894a939aa744dcff203534afc53e3dcec609
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-24 22:40:24 +0900

    パスの表記を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users, controllers: { registrations: 'users/registrations'}
-  
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   resources :users, only: %i[index show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
