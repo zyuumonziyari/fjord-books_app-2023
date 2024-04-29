ed8d21821419d31a787bde6ad91ffb82f7539097
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-29 16:15:39 +0900

    create,destroyアクションの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,28 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-    def create
-        report = Report.find(params[:report_id])
-        message = report.messages.build(message_params)
-        message.user_id = current_user.id
-        if message.save
-        else
-            puts "kabigon"
-        end
-      end
-
-    def destroy
-      Message.find(params[:id]).destroy
-      puts "kabigon"
+  def create
+    if params[:report_id].present?
+      commentable = Report.find(params[:report_id])
+    elsif params[:book_id].present?
+      commentable = Book.find(params[:book_id])
     end
 
-private
-    def message_params
-      params.require(:message).permit(:content)
-    end
+    message = commentable.messages.build(message_params)
+    message.user_id = current_user.id 
+    message.save
+    redirect_to polymorphic_path(commentable), notice: t('controllers.common.notice_create', name: Message.model_name.human)
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+    redirect_to polymorphic_path(message.commentable), notice: t('controllers.common.notice_destroy', name: Message.model_name.human)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
