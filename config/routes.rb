656950124ba61a4226c53e9e711ed1409564e777
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-29 16:15:01 +0900

    ルーティング指定の簡潔化のため、deleteアクション時にルーティングをshallowに

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: %i(index show)
+  
   resources :reports do
-    resources :messages, only: %i(create destroy)
+    resources :messages, only: %i(create), shallow: true
   end
+  resources :messages, only: %i(destroy)
+  
+  resources :books do
+    resources :messages, only: %i(create), shallow: true
+  end
+  resources :messages, only: %i(destroy)
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
