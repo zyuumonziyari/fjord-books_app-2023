cebb405cbcb95e176ebb8504a70d5dbb04f56bb5
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-30 16:24:23 +0900

    不要のdiffを作成してしまったので修復

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,26 +22,39 @@ class BooksController < ApplicationController
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human) }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /books/1 or /books/1.json
   def update
-    if @book.update(book_params)
-      redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human) }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
-    redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human)
+
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
+      format.json { head :no_content }
+    end
   end
 
   private
