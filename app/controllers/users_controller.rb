c5462442144e8447f248db1531dbb4ed2a70d1c9
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-30 16:36:44 +0900

    不要なbeforeアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email_address, :password, :avatar)
-  end
 end
