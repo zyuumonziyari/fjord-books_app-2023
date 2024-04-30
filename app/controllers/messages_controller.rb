863fc475dad73ee8eaabbae5de986e9934150c9e
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-30 13:20:18 +0900

    edit,updateアクションの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,6 +14,25 @@ class MessagesController < ApplicationController
     redirect_to polymorphic_path(commentable), notice: t('controllers.common.notice_create', name: Message.model_name.human)
   end
 
+  def edit
+    @message = Message.find(params[:id])
+    if params[:report_id].present?
+      @commentable = Report.find(params[:report_id])
+    elsif params[:book_id].present?
+      @commentable = Book.find(params[:book_id])
+    end
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    if @message.update(message_params)
+      redirect_to polymorphic_path(@message.commentable), notice: t('controllers.common.notice_update', name: Message.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  
+  end
+
   def destroy
     message = Message.find(params[:id])
     message.destroy
