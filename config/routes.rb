5ea567ae2ed95427364c1b8959f5bc5dadaab21d
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-30 11:44:02 +0900

    edit,updateの追加と、reports,booksを一つにまとめた

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: %i(index show)
-  
-  resources :reports do
-    resources :messages, only: %i(create), shallow: true
+  resources :reports, :books do
+    resources :messages, only: %i(create edit update destroy), shallow: true
   end
-  resources :messages, only: %i(destroy)
-  
-  resources :books do
-    resources :messages, only: %i(create), shallow: true
-  end
-  resources :messages, only: %i(destroy)
-
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
