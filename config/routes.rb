c2dc94218bdda81fb851859d1e53dd44635636ee
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-04-30 16:34:11 +0900

    不要のdiffを作成してしまったので修復

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
+  root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
 end
