7ee3308fcdf078ff1650eef1a671f31aca01f0ba
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-05-06 16:27:58 +0900

    余計なdiffの削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
       format.json { head :no_content }
