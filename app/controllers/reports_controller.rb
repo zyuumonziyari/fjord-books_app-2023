348373fd0ce1431efcd105c652298fedcd3af9a5
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-05-16 21:03:29 +0900

    ページネーション対応

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < ApplicationController
 
   # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.order(:id).page(params[:page])
   end
 
   # GET /reports/1 or /reports/1.json
