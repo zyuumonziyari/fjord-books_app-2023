bd436c6a7857ea3b77d5b56be673c113ad3d89fb
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-03 18:37:40 +0900

    コメント投稿者の名前表記のロジックをモデルファイルから呼び出す

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   validates :content, presence: true
+
+  def display_user_name
+    user.name? ? user.name : user.email
+  end
 end
