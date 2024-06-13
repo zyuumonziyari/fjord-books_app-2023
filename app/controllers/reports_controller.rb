91d178f387bb531774d43f396ac1f79f83ed9669
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-13 21:55:41 +0900

    コメントアウトを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,26 +4,21 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :authorize_user, only: %i[edit update destroy]
 
-  # GET /reports or /reports.json
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
   def show
     comment = Comment.new
     render :show, locals: { report: @report, comment: }
   end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit; end
 
-  # POST /reports or /reports.json
   def create
     @report = current_user.reports.build(report_params)
     if @report.save
@@ -33,7 +28,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
     if @report.update(report_params)
       redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
@@ -42,7 +36,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
@@ -50,12 +43,10 @@ class ReportsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_report
     @report = Report.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def report_params
     params.require(:report).permit(:title, :content, :created_at)
   end
