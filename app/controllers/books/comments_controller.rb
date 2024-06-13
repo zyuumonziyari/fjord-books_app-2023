acc234f43e026cd42bfb3c122f518aa8b32f8ced
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-13 19:17:26 +0900

    render処理のメソッドを各子のコントローラから呼び出すことで、重複を回避した

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -2,14 +2,8 @@
 
 module Books
   class CommentsController < Comments::BaseController
-    def create
-      comment = @commentable.comments.build(comment_params)
-      comment.user_id = current_user.id
-      if comment.save
-        redirect_to polymorphic_path(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-      else
-        render 'books/show', locals: { book: @commentable, comment: comment }, status: :unprocessable_entity  
-      end
+    def render_commentable(comment)
+      render 'books/show', locals: { book: @commentable, comment: comment }, status: :unprocessable_entity  
     end
 
     private
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -2,14 +2,8 @@
 
 module Reports
   class CommentsController < Comments::BaseController
-    def create
-      comment = @commentable.comments.build(comment_params)
-      comment.user_id = current_user.id
-      if comment.save
-        redirect_to polymorphic_path(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-      else
-        render 'reports/show', locals: { report: @commentable, comment: comment }, status: :unprocessable_entity  
-      end
+    def render_commentable(comment)
+      render 'reports/show', locals: { report: @commentable, comment: comment }, status: :unprocessable_entity  
     end
 
     private
