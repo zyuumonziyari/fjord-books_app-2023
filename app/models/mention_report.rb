e6b98c63337a6207a2c5372156e5e382332a2757
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-15 18:39:15 +0900

    inverse ofによる双方向関連付け

--- app/models/mention_report.rb
+++ app/models/mention_report.rb
@@ -1,4 +1,4 @@
 class MentionReport < ApplicationRecord
-  belongs_to :mentioned_report, class_name: "Report", foreign_key: "mentioned_reports_id"
-  belongs_to :mentioning_report, class_name: "Report", foreign_key: "mentioning_reports_id"
+  belongs_to :mentioned_report, class_name: 'Report', foreign_key: 'mentioned_reports_id', inverse_of: :mentioned_relationships
+  belongs_to :mentioning_report, class_name: 'Report', foreign_key: 'mentioning_reports_id', inverse_of: :mentioning_relationships
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,9 +4,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioned_relationships, class_name: "MentionReport", foreign_key: "mentioned_reports_id", dependent: :destroy
+  has_many :mentioned_relationships, class_name: 'MentionReport', foreign_key: 'mentioned_reports_id', dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioned_reports, through: :mentioned_relationships, source: :mentioning_report
-  has_many :mentioning_relationships, class_name: "MentionReport", foreign_key: "mentioning_reports_id", dependent: :destroy  
+  has_many :mentioning_relationships, class_name: 'MentionReport', foreign_key: 'mentioning_reports_id', dependent: :destroy, inverse_of: :mentioning_report
   has_many :mentioning_reports, through: :mentioning_relationships, source: :mentioned_report
 
   validates :title, presence: true
