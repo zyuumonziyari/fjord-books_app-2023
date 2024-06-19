7cc448331159adb3eda37172815b2c4a116e1498
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-19 22:56:03 +0900

    userモデルのモデルテスト作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should display correct user name or email' do
+    user = User.new(name: '', email: 'foo@example.com')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'soichiro'
+    assert_equal 'soichiro', user.name_or_email
+  end
 end
