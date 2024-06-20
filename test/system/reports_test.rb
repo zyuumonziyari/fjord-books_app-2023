6e5f241a69a9d5c0237f9e00e65f9115fca0f4ba
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-06-20 16:49:14 +0900

    reportモデルのCRUD操作、及びログインに対するシステムテストの作成

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,43 +5,49 @@ require 'application_system_test_case'
 class ReportsTest < ApplicationSystemTestCase
   setup do
     @report = reports(:one)
+    @user = users(:one)
+
+    visit root_url
+    fill_in 'Eメール', with: @user.email
+    fill_in 'パスワード', with: 'password'
+    click_on 'ログイン'
+
+    assert_text 'ログインしました'
   end
 
   test 'visiting the index' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    assert_text '日報の一覧'
   end
 
   test 'should create report' do
     visit reports_url
-    click_on 'New report'
+    click_on '日報の新規作成'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: @report.title
+    click_on '登録する'
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text '日報が作成されました'
+    click_on '日報の一覧に戻る'
   end
 
   test 'should update Report' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on 'この日報を編集'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: @report.title
+    click_on '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました'
+    click_on '日報の一覧に戻る'
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_on 'この日報を削除'
 
-    assert_text 'Report was successfully destroyed'
+    assert_text '日報が削除されました'
   end
 end
